1a0e822b8b5567b9af0760ce6915a8bc4949fab4
anh <anhleduy@chotot.vn>
2021-11-05 16:08:00 +0700

    setup dotenv

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-const path = require("path");
+// const path = require("path");
+// require("dotenv").config();
 
 module.exports = {
   reactStrictMode: true,
+  // env: {
+  //   API: process.env.NEXT_PUBLIC_API,
+  // },
   // webpack: (config) => {
   //   config.resolve.modules.push(path.resolve("./"));
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import styles from "@styles/Home.module.scss";
 
+const user = {
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  pass: process.env.DB_PASS,
+};
+console.log(user);
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <p id={styles.title}>Duy Anh</p>
     </div>
   );
+}
+
+export async function getAccountInfo() {
+  return {
+    props: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  };
 }
